f9d021716e44428640d20bacf881ddde31a9ab15
wangxin <wangxin@ifeng.com>
2014-07-04 17:20:27 +0800

    Initial commit

---
+++ src/main/java/test.java
@@ -0,0 +1,10 @@
+/**
+ * Created by wangxin on 2014/7/4.
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("add11");
+
+        System.out.println("a");
+    }
+}
